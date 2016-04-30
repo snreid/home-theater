33ba1ad835ed8d7adcf8d3c21a9599e44ca706dd
Sarah Reid <sreid@covermymeds.com>
2016-04-30 09:05:47 -0400

    Added seed for locations, added scope to order them in the UI.

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,4 @@
 class Location < ActiveRecord::Base
   has_many :dvds
+  scope :ordered, -> { order(:shelf, :row, :stack) }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+puts "Adding default locations"
+MAX_SHELVES = 4
+MAX_ROWS = 5
+MAX_STACKS = 3
+
+MAX_SHELVES.times do |shelf|
+  MAX_ROWS.times do |row|
+    MAX_STACKS.times do |stack|
+      Location.find_or_create_by(shelf: shelf+1, row: row+1, stack: stack+1)
+    end
+  end
+end
